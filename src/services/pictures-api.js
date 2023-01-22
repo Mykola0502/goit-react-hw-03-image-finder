15b0e2febb9cfdde69f9fb526e390133224ae055
Mykola Saliuk <haker0502@ukr.net>
2023-01-22 21:55:44 +0200

    Update pictures-api.js

--- src/services/pictures-api.js
+++ src/services/pictures-api.js
@@ -1,12 +1,43 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-// axios.defaults.headers.common['x-api-key'] =
-//   '31583377-fa4c6976355a1f179c9a11dc6';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+// const API_KEY = '31583377-fa4c6976355a1f179c9a11dc6';
+// const options = 'image_type=photo&orientation=horizontal&per_page=12';
 
-export const fetchPictures = async () => {
-  const response = await axios.get(
-    '?q=cat&page=1&key=31583377-fa4c6976355a1f179c9a11dc6&image_type=photo&orientation=horizontal&per_page=12'
-  );
+// export const fetchPictures = async (query, page) => {
+//   const response = await axios.get(
+//     `?q=${query}&page=${page}&key=${API_KEY}&${options}`
+//   );
+//   return response.data;
+// };
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+export const fetchPictures = async (query, page) => {
+  const response = await axios.get('', {
+    params: {
+      key: '31583377-fa4c6976355a1f179c9a11dc6',
+      q: query,
+      page: page,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      per_page: 12,
+    },
+  });
   return response.data;
 };
+
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+// export const fetchPictures = async arg => {
+//   const response = await axios.get('https://pixabay.com/api/', {
+//     params: {
+//       key: '31583377-fa4c6976355a1f179c9a11dc6',
+//       q: arg,
+//       page: 1,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       per_page: 12,
+//     },
+//   });
+//   return response.data;
+// };
